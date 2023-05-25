3b787df48171c26ec108ca8cdd220fd56830c47e
Rishabh Sinha <rsinha@tekion.com>
2023-05-25 22:59:17 +0530

    feat: first commit

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,13 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get(  '*', (req, res) => {
-
-
+app.get('*', (req, res) => {
   res.send({});
 });
 
 app.listen(3000, () => {
-
-  console.log("Listening on Port: 3000");
+  console.log('Listening on Port: 3000');
 });
