c935512fc3d5cd64d8b3f57e3b597018c713ed1b
Rishabh Sinha <rsinha@tekion.com>
2023-05-27 22:32:23 +0530

    feat: second test

--- auth/src/routes/__tests__/signup.test.ts
+++ auth/src/routes/__tests__/signup.test.ts
@@ -1,18 +1,22 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-beforeAll(() => {
-  // start DB
-});
+// beforeAll(() => {
+//   // start DB
+// });
 
-beforeEach(() => {
-  // clean up DB
-});
+// beforeEach(() => {
+//   // clean up DB
+// });
 
-afterAll(() => {
-  // close DB Connection
-});
+// afterAll(() => {
+//   // close DB Connection
+// });
 
-it('Responds with status 200', async () => {
+// it('should return 405 for non-post request to login route ', async () => {
+//     await request(app).get('/').expect(200);
+//   });
+
+it('should return 422 if email is invalid ', async () => {
   await request(app).get('/').expect(200);
 });
